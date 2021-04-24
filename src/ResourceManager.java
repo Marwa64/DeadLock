1df6d4f3d6a9fa2d9d827a13e5b5e978db693cd8
Marwa64 <farabi.marwa@gmail.com>
2021-04-25 00:43:05 +0200

    request and safe state

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -22,20 +22,64 @@ public class ResourceManager {
 		}
 	}
 	
-	public void allocate() {
+	public void request(int process, int [] resources) throws InterruptedException {
+		for (int i = 0; i < m; i++) {
+			if (resources[i] > need[process][i]) {
+				System.out.println("Process #" + process + " requested more of resource #" + i + " than it needs");
+				return;
+			}
+			if (resources[i] > available[i]) {
+				System.out.println("Not enough of resource #" + i + " available");
+				return;
+			}
+		}
+		System.out.println("\nProcess #" + process + " did not exceed the number of resources it needs");
+		System.out.println("\nThere are enough resources available\n");
+		
+		for (int i = 0; i < m; i++) {
+			available[i] -= resources[i];
+			need[process][i] -= resources[i];
+			allocated[process][i] += resources[i];
+		}
+		
+		if (safeState()) {
+			System.out.println("This request is safe");
+		} else {
+			for (int i = 0; i < m; i++) {
+				available[i] += resources[i];
+				need[process][i] += resources[i];
+				allocated[process][i] -= resources[i];
+			}
+			System.out.println("This request is not safe");
+		}
 		
 	}
 	
-	public void request(int process) {
+	boolean safeState() {
+		int [] work = this.available;
+		boolean [] finish = new boolean [n];
+		for (int i = 0; i < n; i++) {
+			finish[i] = false;
+		}
 		
+		int process = 0, resource = 0;
+		while (finish[process] == false && need[process][resource] <= work[resource]) {
+			// release resources
+			work[resource] += allocated[process][resource];
+			finish[process] = true;
+			process++; resource++;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (finish[i] == false)
+				return false;
+		}
+		return true;
 	}
 	
 	public void release(int process) {
 		
 	}
 	
-	boolean safetyState() {
-		return true;
-	}
 	
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,15 @@
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		int processes = 5, resources = 3;
 		int [] available = {2, 5, 4};
 		int [][] maximum = {{5,5,3},{3,2,3},{8,0,2},{4,2,2},{5,3,5}};
 		int [][] allocated = {{1,1,0},{2,0,0},{2,0,1},{2,0,2},{0,1,2}};
 		
 		ResourceManager banker = new ResourceManager(processes, resources, available, allocated, maximum);
-		banker.allocate();
+		int [] req = {2, 0, 1};
+		banker.request(4, req);
 	}
 
 }
