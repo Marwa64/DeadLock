b6a6660e8fdd8d5fe31c33a0ad898eb583afff32
Marwa64 <farabi.marwa@gmail.com>
2021-04-24 21:21:21 +0200

    Init structure

---
+++ src/ResourceManager.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ResourceManager {
+	int n, m;
+	int [] available;
+	int [][] allocation;
+	int [][] need;
+	
+	void setupData() {
+		
+	}
+	
+	public void allocate() {
+		
+	}
+	
+	boolean safetyState() {
+		return true;
+	}
+}
