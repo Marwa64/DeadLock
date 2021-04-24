90cb9af98ed678a0f30b1c92ed448f500f22d71c
Marwa64 <farabi.marwa@gmail.com>
2021-04-24 23:51:37 +0200

    calculate need matrix

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,20 +1,41 @@
-import java.util.ArrayList;
 
 public class ResourceManager {
 	int n, m;
 	int [] available;
-	int [][] allocation;
+	int [][] max;
+	int [][] allocated;
 	int [][] need;
 	
-	void setupData() {
+	public ResourceManager(int processes, int resources, int [] available, int [][] allocated, int [][] maximum) {
+		this.n = processes;
+		this.m = resources;
+		this.available = available;
+		this.allocated = allocated;
+		this.max = maximum;
 		
+		this.need = new int [n][m];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				need[i][j] = max[i][j] - allocated[i][j];
+			}
+		}
 	}
 	
 	public void allocate() {
 		
 	}
 	
+	public void request(int process) {
+		
+	}
+	
+	public void release(int process) {
+		
+	}
+	
 	boolean safetyState() {
 		return true;
 	}
+	
 }
