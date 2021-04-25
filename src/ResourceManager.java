394b5323b030cd15876ea4ba8c6e9e881a4aff83
Marwa64 <farabi.marwa@gmail.com>
2021-04-25 02:22:46 +0200

    fix safeState, release, test

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,85 +1,140 @@
 
 public class ResourceManager {
-	int n, m;
+	int p, r;
 	int [] available;
 	int [][] max;
 	int [][] allocated;
 	int [][] need;
 	
 	public ResourceManager(int processes, int resources, int [] available, int [][] allocated, int [][] maximum) {
-		this.n = processes;
-		this.m = resources;
+		this.p = processes;
+		this.r = resources;
 		this.available = available;
 		this.allocated = allocated;
 		this.max = maximum;
 		
-		this.need = new int [n][m];
+		this.need = new int [p][r];
 		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+		for (int i = 0; i < p; i++) {
+			for (int j = 0; j < r; j++) {
 				need[i][j] = max[i][j] - allocated[i][j];
 			}
 		}
 	}
 	
+	public void printData() {
+		System.out.println("Allocation matrix");
+		for(int i = 0; i < p; i++) {
+			for(int j = 0; j < r; j++) {
+				System.out.print(allocated[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+
+		System.out.println("Maximum matrix");
+		for(int i = 0; i < p; i++) {
+			for(int j = 0; j < r; j++) {
+				System.out.print(max[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+
+		System.out.println("Available matrix");
+		for(int j = 0; j < r; j++) {
+			System.out.print(available[j] + " ");
+		}
+		System.out.println();
+			
+		System.out.println("Need matrix");
+		for(int i = 0; i < p; i++) {
+			for(int j = 0; j < r; j++) {
+				System.out.print(need[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("\n---------------------------------------\n");
+	}
+	
 	public void request(int process, int [] resources) throws InterruptedException {
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < r; i++) {
 			if (resources[i] > need[process][i]) {
-				System.out.println("Process #" + process + " requested more of resource #" + i + " than it needs");
+				System.out.println("\nProcess #" + process + " requested more of resource #" + i + " than it needs");
 				return;
 			}
 			if (resources[i] > available[i]) {
-				System.out.println("Not enough of resource #" + i + " available");
+				System.out.println("\nNot enough of resource #" + i + " available");
 				return;
 			}
 		}
 		System.out.println("\nProcess #" + process + " did not exceed the number of resources it needs");
 		System.out.println("\nThere are enough resources available\n");
 		
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < r; i++) {
 			available[i] -= resources[i];
 			need[process][i] -= resources[i];
 			allocated[process][i] += resources[i];
 		}
 		
 		if (safeState()) {
-			System.out.println("This request is safe");
+			System.out.println("This request is safe, resources have been allocated\n");
 		} else {
-			for (int i = 0; i < m; i++) {
+			for (int i = 0; i < r; i++) {
 				available[i] += resources[i];
 				need[process][i] += resources[i];
 				allocated[process][i] -= resources[i];
 			}
-			System.out.println("This request is not safe");
+			System.out.println("This request is not safe, resourced allocation has been denied\n");
 		}
 		
 	}
 	
+	boolean checkResources(int process) {
+		for (int j = 0; j < r; j++) {
+			if (need[process][j] > available[j]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	boolean safeState() {
 		int [] work = this.available;
-		boolean [] finish = new boolean [n];
-		for (int i = 0; i < n; i++) {
+		boolean [] finish = new boolean [p];
+		for (int i = 0; i < p; i++) {
 			finish[i] = false;
 		}
 		
-		int process = 0, resource = 0;
-		while (finish[process] == false && need[process][resource] <= work[resource]) {
-			// release resources
-			work[resource] += allocated[process][resource];
-			finish[process] = true;
-			process++; resource++;
+		for (int i = 0; i < p; i++) {
+			if (finish[i] == false && checkResources(i)) {
+				for (int j = 0; j < r; j++) {
+					work[j] += allocated[i][j];
+				}
+				finish[i] = true;
+				i = 0;
+			}
 		}
 		
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < p; i++) {
 			if (finish[i] == false)
 				return false;
 		}
 		return true;
 	}
 	
-	public void release(int process) {
-		
+	public void release(int process, int [] resources) {
+		for (int i = 0; i < r; i++) {
+			if (resources[i] > allocated[process][i]) {
+				System.out.println("\nProcess #" + process + " can't release more of resource #" + i + " than it has allocated");
+				return;
+			}
+		}
+		for (int i = 0; i < r; i++) {
+			allocated[process][i] -= resources[i];
+			available[i] += resources[i];
+		}
+		System.out.println("\nResources have been released\n");
 	}
-	
-	
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,72 @@
+import java.util.Scanner;
 
 public class Test {
+	static Scanner sc = new Scanner(System.in);
+	
+	static int processes = 5;
+	static int resources = 3;
+	static int [] available = {2, 5, 4};
+	static int [][] maximum = {{5,5,3},{3,2,3},{8,0,2},{4,2,2},{5,3,5}};
+	static int [][] allocated = {{1,1,0},{2,0,0},{2,0,1},{2,0,2},{0,1,2}};
+	
+	static void getInput(){
+		System.out.print("Enter no. of processes: ");
+		processes = sc.nextInt();  
+		System.out.print("Enter no. resources: ");
+		resources = sc.nextInt();  
+		maximum = new int[processes][resources];
+		allocated = new int[processes][resources];
+		available = new int[resources];
+
+		System.out.println("Enter allocation matrix");
+		for(int i = 0; i < processes; i++)
+			for(int j = 0; j < resources; j++)
+				allocated[i][j] = sc.nextInt();  //allocation matrix
+
+		System.out.println("Enter maximum matrix");
+		for(int i = 0; i < processes; i++)
+			for(int j = 0; j < resources; j++)
+				maximum[i][j] = sc.nextInt();  //max matrix
+
+		System.out.println("Enter available matrix");
+		for(int j = 0; j < resources; j++)
+			available[j] = sc.nextInt();  //available matrix
+	}
 
 	public static void main(String[] args) throws InterruptedException {
-		int processes = 5, resources = 3;
-		int [] available = {2, 5, 4};
-		int [][] maximum = {{5,5,3},{3,2,3},{8,0,2},{4,2,2},{5,3,5}};
-		int [][] allocated = {{1,1,0},{2,0,0},{2,0,1},{2,0,2},{0,1,2}};
-		
+		System.out.println("Would you like to: \n 1) Enter your own data \n 2) Use the prepared test data? \nYour choice: ");
+		int choice = sc.nextInt();
+		if (choice == 1) {
+			getInput();
+		}
 		ResourceManager banker = new ResourceManager(processes, resources, available, allocated, maximum);
-		int [] req = {2, 0, 1};
-		banker.request(4, req);
+		
+		banker.printData();
+		
+		String operation;
+		int [] resourcesInput = new int [resources];
+		int processInput;
+		do {
+			System.out.print("Operation (RQ / RL / Quit) : ");
+			sc.nextLine();
+			operation = sc.nextLine();
+			if (!operation.equalsIgnoreCase("quit")) {
+				System.out.print("Process : ");
+				processInput = sc.nextInt();
+				System.out.print("Resource : ");
+				for (int i = 0; i < resources; i++) {
+					resourcesInput[i] = sc.nextInt();
+				}
+				if (operation.equalsIgnoreCase("rq")) {
+					banker.request(processInput, resourcesInput);
+				}
+				if (operation.equalsIgnoreCase("rl")) {
+					banker.release(processInput, resourcesInput);
+				}
+			}
+			System.out.println("\n---------------------------------------\n");
+		} while (operation != "quit");
+		sc.close();
 	}
 
 }
